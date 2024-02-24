0f6cccad34cd492c8277766d4d30139e5cfa5960
Andrés Martínez <arkantos890@gmail.com>
2024-02-24 13:21:17 -0600

    Initial Commit

---
+++ HelloiOS-sui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HelloiOS-sui
+//
+//  Created by Andrés Alfonso Martínez González on 24/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloiOS-sui/HelloiOS_suiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloiOS_suiApp.swift
+//  HelloiOS-sui
+//
+//  Created by Andrés Alfonso Martínez González on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloiOS_suiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
